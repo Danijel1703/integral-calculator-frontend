d9507429f442bdcba993017823c4313a5aba1f87
Danijel1703 <danci.dj@gmail.com>
2022-09-11 13:05:58 +0200

    change base url

--- src/API/CalculatorService.js
+++ src/API/CalculatorService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'http://danijel.pythonanywhere.com'
 
 class CalculatorService {
     async solveWithAll(equation, downLimit, upperLimit, subintervals) {
