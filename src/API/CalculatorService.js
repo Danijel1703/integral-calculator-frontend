d9add280224b069e5f4fec87159ec5b9999e4595
Danijel1703 <danci.dj@gmail.com>
2022-09-15 09:50:51 +0200

    Update CalculatorService.js

--- src/API/CalculatorService.js
+++ src/API/CalculatorService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'https://danijel.pythonanywhere.com'
 
 class CalculatorService {
     async solveWithAll(equation, downLimit, upperLimit, subintervals) {
